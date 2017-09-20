95cec4bc31316b0893fad95d95b356c9d4540480
PW486 <ooqwe486@gmail.com>
2017-09-20 15:15:26 +0900

    Kieehack

--- kieehack.py
+++ kieehack.py
@@ -6,10 +6,9 @@ import os
 def main(argv):
 	commit_num = int(argv[0])
 	Kieek = "Kieek"
-	for _ in range(commit_num):
+	for _ in range(commit_num-1):
 		subprocess.call("echo '" + Kieek + str(randint(0, 1000000)) +"' > Kieek.txt; git add Kieek.txt; GIT_AUTHOR_NAME='" + Kieek + "' GIT_AUTHOR_EMAIL='" + Kieek + "' GIT_COMMITTER_NAME='" + Kieek + "' GIT_COMMITTER_EMAIL='" + Kieek + "' git commit -m 'Kieek';", shell=True)
 
-	subprocess.call("git push;", shell=True)
 	subprocess.call("git rm Kieek.txt; GIT_AUTHOR_NAME='" + Kieek + "' GIT_AUTHOR_EMAIL='" + Kieek + "' GIT_COMMITTER_NAME='" + Kieek + "' GIT_COMMITTER_EMAIL='" + Kieek + "' git commit -m 'Kieek'; git push;", shell=True)
 
 if __name__ == "__main__":
